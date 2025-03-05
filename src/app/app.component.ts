81144a2e538dd6e08d3cf32bd81f0ec628eae85e
Saurabh Saraswat <saurabh45215@gmail.com>
2025-03-05 15:06:24 +0530

    [Saurabh] Add Two Way Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ FormsModule],
   // template: `<h2>Saurabh</h2>`,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  username = ""; //two way binding
   imgSrc = 'new.png';//property binding
   title = 'ANGULAR STARTER';
   url = 'https://www.bridgelabz.com';

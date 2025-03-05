971deac377839518da2834058efff4bf8de2b5c7
Saurabh Saraswat <saurabh45215@gmail.com>
2025-03-05 15:49:58 +0530

    [Saurabh] Add Validate User Input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,22 +3,26 @@ import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   imports: [ FormsModule],
+  standalone: true,
   // template: `<h2>Saurabh</h2>`,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  username = ""; //two way binding
+  username: string = ""; //two way binding
+  usernameerror: string = ""; //two way binding
   imgSrc = 'new.png';//property binding
-  title = 'ANGULAR STARTER';
+  title = 'ANGULAR STARTER By ';
   url = 'https://www.bridgelabz.com';
 
-  ngOnInit() {
-    this.title = 'ANGULAR STARTER By Saurabh';
+  onClick($event: any) {
+    console.log('Button Clicked'+$event);
+    window.open(this.url, '_blank');
   }
 
-  onClick() {
-    console.log('Button Clicked');
-    window.open(this.url, '_blank');
+  onEvent() {
+    // Ensure the userName starts with a capital letter and has a minimum of 3 letters
+    const regex = /^[A-Z][A-Za-z]{2,}$/;
+    this.usernameerror = regex.test(this.username) ? '' : 'Invalid UserName';
   }
 }
